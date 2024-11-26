4e3c4235636e7cdf28e6ddca13a4078550f0fb79
Anatooolio <margach1465@gmail.com>
2024-11-26 23:51:48 +0300

    Release 1

--- index.js
+++ index.js
@@ -11,9 +11,50 @@ class Person {
 
 const person1 = new Person('Толя', 22)
 const person2 = new Person('Арина', 21)
-console.log(person1)
-console.log(person1.sayHello())
-console.log(person2)
-console.log(person2.sayHello())
+// console.log(person1)
+// console.log(person1.sayHello())
+// console.log(person2)
+// console.log(person2.sayHello())
 
 //-----------------------------------------------------------------------------------------
+
+class Calculator {
+	constructor(params) {
+		//Конструктор принимает параметры и мы этим параметрам передаем значения num1 и num2
+		this.num1 = params.num1
+		this.num2 = params.num2
+	}
+
+	add() {
+        let res = this.num1 + this.num2
+		return `Сумма: ${res}`
+	}
+    
+	subtract() {
+        let res = this.num1 - this.num2
+        return `Разность: ${res}`
+	}
+    
+	multiple() {
+        let res = this.num1 * this.num2
+        return `Произведение: ${res}`
+    }
+    
+    divide() {
+        let res = this.num1 / this.num2
+        return `Частное: ${res}`
+    }
+}
+
+//Создаем первые значения с помощью объекта
+const firstValues = {
+	num1: 2,
+	num2: 5,
+}
+
+const res1 = new Calculator(firstValues) //Создаем экземпляр, принимающий объект
+console.log(res1.add())
+console.log(res1.subtract())
+console.log(res1.multiple())
+console.log(res1.divide())
+
